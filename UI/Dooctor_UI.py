0ae79eeae4784475466a8e953870dc29ba9b5562
C.W. Chien <dppss92132@gmail.com>
2022-01-05 22:26:06 +0800

    Add program UI
    I don't think I need to do the UI part to practice but teacher ask us to 
    do so.....

---
+++ UI/Dooctor_UI.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Dooctor.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(580, 120, 111, 19))
+        self.label_3.setObjectName("label_3")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(580, 190, 111, 19))
+        self.label_5.setObjectName("label_5")
+        self.horizontalSlider = QtWidgets.QSlider(self.centralwidget)
+        self.horizontalSlider.setGeometry(QtCore.QRect(40, 530, 481, 16))
+        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider.setObjectName("horizontalSlider")
+        self.graphicsView_5 = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView_5.setGeometry(QtCore.QRect(380, 350, 151, 101))
+        self.graphicsView_5.setObjectName("graphicsView_5")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(60, 40, 136, 27))
+        self.pushButton.setObjectName("pushButton")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(360, 480, 66, 19))
+        self.label_2.setObjectName("label_2")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(580, 140, 66, 16))
+        self.label_4.setObjectName("label_4")
+        self.graphicsView_2 = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView_2.setGeometry(QtCore.QRect(290, 120, 171, 192))
+        self.graphicsView_2.setObjectName("graphicsView_2")
+        self.graphicsView_3 = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView_3.setGeometry(QtCore.QRect(90, 350, 111, 101))
+        self.graphicsView_3.setObjectName("graphicsView_3")
+        self.graphicsView_4 = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView_4.setGeometry(QtCore.QRect(250, 350, 121, 101))
+        self.graphicsView_4.setObjectName("graphicsView_4")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(640, 140, 87, 27))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setGeometry(QtCore.QRect(580, 270, 101, 19))
+        self.label_6.setObjectName("label_6")
+        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(80, 110, 171, 192))
+        self.graphicsView.setObjectName("graphicsView")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(150, 470, 66, 19))
+        self.label.setObjectName("label")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(280, 40, 117, 27))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(500, 30, 201, 27))
+        self.pushButton_3.setObjectName("pushButton_3")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 24))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_3.setText(_translate("MainWindow", "Type: Fracture"))
+        self.label_5.setText(_translate("MainWindow", "Current Image:"))
+        self.pushButton.setText(_translate("MainWindow", "Open Image Folder"))
+        self.label_2.setText(_translate("MainWindow", "Result"))
+        self.label_4.setText(_translate("MainWindow", "Predict:"))
+        self.pushButton_4.setText(_translate("MainWindow", "Fracture"))
+        self.label_6.setText(_translate("MainWindow", "Folder (mean):"))
+        self.label.setText(_translate("MainWindow", "Image"))
+        self.pushButton_2.setText(_translate("MainWindow", "Detect Scaphoid"))
+        self.pushButton_3.setText(_translate("MainWindow", "Classify and Detect Fracture"))
+
