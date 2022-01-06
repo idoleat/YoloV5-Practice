710034ccd38e8c18b4c693e30f19170fa2c55464
C.W. Chien <dppss92132@gmail.com>
2022-01-06 19:51:20 +0800

    Doocotor supports loading images from a diretory(folder)
    There isn't QFileDialog in Qt Designer (Why?) so the dialog is defined 
    in the Dooctor.py
    Python is wierd....

--- UI/Dooctor.py
+++ UI/Dooctor.py
@@ -1,18 +1,44 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QFileDialog
+from PIL import Image
+import os
 import sys
 import Dooctor_UI
+
+
+def loadImages(path):
+    imagesList = os.listdir(path)
+    loadedImages = []
+    for image in imagesList:
+        if os.path.isdir(path + '/' + image):
+            continue
+        img = Image.open(path + '/' + image)
+        loadedImages.append(img)
+
+    return loadedImages
+
 
 class Dooctor(QtWidgets.QMainWindow, Dooctor_UI.Ui_MainWindow):
     def __init__(self, parent=None):
         super(Dooctor, self).__init__(parent)
         self.setupUi(self)
+        self.OpenImageFolder.clicked.connect(self.getDirectory)
+
+    def getDirectory(self):
+        dialog = QFileDialog(self)
+        dialog.setFileMode(QFileDialog.Directory)
+
+        if dialog.exec_():
+            dirPath = dialog.selectedFiles()
+            self.imgs = loadImages(dirPath[0])
+
 
 def main():
     app = QApplication(sys.argv)
     form = Dooctor()
     form.show()
     app.exec_()
+
 
 if __name__ == '__main__':
     main()
