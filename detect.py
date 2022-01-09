de8d015ac83b8d0c5299565a0ce965c0031e4257
C.W. Chien <dppss92132@gmail.com>
2022-01-09 21:58:15 +0800

    Add new data
    visible hands

--- detect.py
+++ detect.py
@@ -11,16 +11,18 @@ img2 = Image.open('./data/test2.jpg')
 imgs = [img1, img2]  # batch of images
 
 # Inference
+model.eval()
 results = model(imgs)  # includes NMS
 
 # Results
+print(results)
 results.print()
-results.save()
-crops = results.crop(save=True)  # cropped detections dictionary
+results.save('./sdfsdf')
+crops = results.crop('./sdfsdf')  # cropped detections dictionary
 # results.show()
 print('\n===img1 predictions (tensor)===')
 print(results.xyxy[0])  # img1 predictions (tensor)
 print('========\n')
 print('===img1 predictions (pandas)===')
-print(results.pandas().xyxy[0])  # img1 predictions (pandas)
+print(results.pandas().xyxy[0].xmin[0])  # img1 predictions (pandas)
 print('========\n')
